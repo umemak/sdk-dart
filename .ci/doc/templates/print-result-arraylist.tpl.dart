8dbb8e1a449e8172efbbef7d01b44052768b780e
Shiranuit <shiranui007@gmail.com>
2021-06-15 12:31:24 +0200

    templates should now instanciate HTTP or Websocket  protocol based on env variable

--- .ci/doc/templates/print-result-arraylist.tpl.dart
+++ .ci/doc/templates/print-result-arraylist.tpl.dart
@@ -3,14 +3,30 @@ import 'dart:io';
 import 'package:kuzzle/kuzzle.dart';
 
 void main() async {
+KuzzleProtocol protocol;
+  if (Platform.environment['SNIPPET_PROTOCOL'] == 'http') {
+    protocol = HttpProtocol(
+      Uri(
+        scheme: 'http',
+        host: 'kuzzle',
+        port: 7512,
+      )
+    );
+  } else {
+    protocol = WebSocketProtocol(
+      Uri(
+        scheme: 'ws',
+        host: 'kuzzle',
+        port: 7512,
+      )
+    );
+  }
+
   final kuzzle = Kuzzle(
-    WebSocketProtocol(Uri(
-    scheme: 'ws',
-    host: 'kuzzle',
-    port: 7512,
-  )),
+    protocol,
     offlineMode: OfflineMode.auto,
   );
+  
   await kuzzle.connect();
   [snippet-code]
   print(result.values);
